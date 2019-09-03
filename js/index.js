09e473a895e2b0dfc8405c0f4d8407f774f79473
ofega <fegaeze@gmail.com>
2019-09-03 03:21:38 +0100

    implemented drag and drop for banner image

--- js/index.js
+++ js/index.js
@@ -19,3 +19,27 @@ navlinks.forEach(link => {
         })
     })
 })
+
+
+/* === Drag & Drop === */
+const banner = document.querySelector('.intro > img');
+
+banner.setAttribute('id', 'dragTest');
+banner.setAttribute('draggable', 'true');
+
+banner.addEventListener('dragstart', e => {
+    e.dataTransfer.setData('text/plain', event.target.id);
+})
+document.body.addEventListener('dragover', e => {
+    e.preventDefault();
+});
+document.body.addEventListener('drop', e => {
+    const id = e.dataTransfer.getData('text');
+    const draggableElement = document.getElementById(id);
+    const dropzone = e.target;
+    
+    dropzone.appendChild(draggableElement);
+    e.dataTransfer.clearData();
+});
+
+
